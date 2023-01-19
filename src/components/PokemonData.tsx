884a64c69bfe8ea187d7d7cba35e378260850da6
williamcsa04 <williamcaetano14@gmail.com>
2023-01-18 21:32:36 -0300

    :sparkles: Added more data

--- src/components/PokemonData.tsx
+++ src/components/PokemonData.tsx
@@ -21,41 +21,57 @@ export function PokemonData() {
         }
     }
 
-    console.log(history)
-
     return (
         <>
             <SearchForm changeCurrent={changeCurrent} changeHistory={changeHistory} />
-            <div class="flex">
-                <div class="flex flex-col">
-                    <p class="font-weight">Histórico de pesquisa</p>
-                    {
-                        history.length ? (
-                            <ol class="list-none">
-                                {
-                                    history.map((data) => (
-                                        <li>{data.name}</li>
-                                    ))
-                                }
-                            </ol>
-                        ) : (
-                            <span>Não há pesquisas feitas</span>
-                        )
-                    }
-                </div>
+            <div class="flex space-x-2 mt-8 capitalize">
+                {
+                    !!history.length && (
+                        <div class="flex flex-col">
+                            <p class="font-bold">Histórico de pesquisa</p>
+                            {
+                                <ol class="list-none">
+                                    {
+                                        history.map((data) => (
+                                            <li>{data.name}</li>
+                                        ))
+                                    }
+                                </ol>
+                            }
+                        </div>
+                    )
+                }
                 {
                     current && (
                         <div class="flex">
-                            <img src={current.sprites.front_default} alt={current.name} />
+                            <img src={current.sprites.front_default} alt={current.name} class="w-24 h-24 m-auto" />
                             <div>
-                                <h2 class="capitalize font-bold">{current.name}</h2>
-                                <ul>
-                                    {
-                                        current.abilities.map((skill) => (
-                                            <li>{skill.ability.name}</li>
-                                        ))
-                                    }
-                                </ul>
+                                <h2 class="capitalize font-bold text-center">{current.name}</h2>
+                                <div class="flex space-x-12 capitalize">
+                                    <div class="flex flex-col">
+                                        <ul>
+                                            {
+                                                current.abilities.map((skill) => (
+                                                    <li>{skill.ability.name}</li>
+                                                ))
+                                            }
+                                        </ul>
+                                        <ul class="mt-auto">
+                                            {
+                                                current.types.map((type) => (
+                                                    <li>{type.type.name}</li>
+                                                ))
+                                            }
+                                        </ul>
+                                    </div>
+                                    <ul>
+                                        {
+                                            current.stats.map((stat) => (
+                                                <li>{stat.stat.name}: {stat.base_stat}</li>
+                                            ))
+                                        }
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     )
